79ddd722f23c0fcee81ce1d374536e151e012ada
Joana Palero <joana.palero24@gmail.com>
2018-08-01 00:25:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,4 +2,10 @@ class Application
   def call(env)
     resp = Rack::Response.new 
     if Time.hour.now < 12
-      
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
+    resp.finish
+  end
+end
